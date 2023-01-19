b0f0e5dd742e8768cd7b7b1077ca31f16ce0e5bb
Tiago Neves Leao Costa Lino <tiagobrasil18@gmail.com>
2023-01-19 12:14:24 -0300

    API Rest with basic POST and GET Methods ajust

--- src/main/java/org/acme/entity/Customer.java
+++ src/main/java/org/acme/entity/Customer.java
@@ -1,18 +1,14 @@
 package org.acme.entity;
 
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @NoArgsConstructor
 public class Customer {
 
@@ -28,16 +24,4 @@ public class Customer {
 
     private String email;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Customer customer = (Customer) o;
-        return id != null && Objects.equals(id, customer.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
